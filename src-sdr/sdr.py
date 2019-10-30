69b8eb2f0a429ae1877ed96fed4530dd463e86fa
Takashi Kojo <kojo@wolfssl.com>
2019-10-30 18:07:52 +0900

    Change dir name

--- src-sdr/sdr.py
+++ src-sdr/sdr.py
@@ -2,19 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as anm
 import itertools
+from scipy.fftpack import fft
 
 def _update(frame):
     line = input()
     print("==INPUT ==")
     str_data = line.split(",")
     data = list(map(float, str_data))
+    N = len(data)
+    T = 1 / N
+    yf = fft(data)
+    xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
     plt.cla()
-    plt.plot(data[0:100])   
+    plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
 
 params = {
     'fig': plt.figure(figsize=(6, 3)),
     'func': _update,
-    'interval': 10, # mSec
+    'interval': 30, # mSec
     'frames': itertools.count(0, 1),  # Frame number iterator
 }
 
