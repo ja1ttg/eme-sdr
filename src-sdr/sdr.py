df50a6e1721f411fdc6b4dc52719bef26b12fd41
Takashi Kojo <kojo@wolfssl.com>
2019-10-30 19:45:56 +0900

    primary FFT

--- src-sdr/sdr.py
+++ src-sdr/sdr.py
@@ -5,25 +5,26 @@ import itertools
 from scipy.fftpack import fft
 
 def _update(frame):
-    line = input()
-    print("==INPUT ==")
-    str_data = line.split(",")
-    data = list(map(float, str_data))
+    data = list(map(float, input().split(",")))
     N = len(data)
     T = 1 / N
     yf = fft(data)
     xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
     plt.cla()
+    plt.title('FFT')
+    plt.xlabel('Frequency')
+    plt.ylabel('Signal level')
+    plt.yscale('log')
     plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
+    plt.grid()
 
 params = {
     'fig': plt.figure(figsize=(6, 3)),
     'func': _update,
-    'interval': 30, # mSec
+    'interval': 10, # mSec
     'frames': itertools.count(0, 1),  # Frame number iterator
 }
 
 anim = anm.FuncAnimation(**params)
-data = []
-plt.plot(data)   
+
 plt.show()
